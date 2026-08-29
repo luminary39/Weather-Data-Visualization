f3e32dafdb4dad84dd22531fce96a731caa7657f
luminary39 <sakibsadman546@gmail.com>
2026-08-30 05:51:40 +0600

    can show dhaka weather figure

--- main.py
+++ main.py
@@ -4,15 +4,50 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import os
 
-# 1. Get weather data
-today = datetime.now()
-week_ago = today - timedelta(days=7)
-start_date = week_ago.strftime("%Y-%m-%d")
-end_date = today.strftime("%Y-%m-%d")
+# -----------------------
+# Get weather data
+# -----------------------
 
-url = f"https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min"
-response = requests.get(url)
-data = response.json()
+def get_weather_data(latitude, longitude):
+    today = datetime.now() 
+    week_ago = today - timedelta(days=7)
+    start_date = week_ago.strftime("%Y-%m-%d")
+    end_date = today.strftime("%Y-%m-%d")
+
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+# ---------------------------------------------------------
+# Setting-up Coordinates of 8 divisional cities in a Dict
+# ---------------------------------------------------------
+
+cities = {
+    "Dhaka": (23.7289, 90.3944),
+    "Chattogram": (22.3350, 91.8325),
+    "Rajshahi": (24.3667, 88.6000),
+    "Khulna": (22.8167, 89.5500),
+    "Barishal": (22.70497, 90.370127),
+    "Sylhet": (24.9000, 91.8667),
+    "Rangpur": (25.6496, 89.2369),
+    "Mymensingh": (24.6988, 90.4287)
+}
+
+
+
+# ------------------------------------------------
+# gETTING WEATHER DATA IN A VARIABLE
+# ------------------------------------------------
+
+latitude, longitude= cities["Dhaka"]
+data = get_weather_data(latitude=latitude, longitude=longitude)
+
+
+# ------------------------------------------------
+# process, visualize and save data in a file
+# ------------------------------------------------
+
 
 # 2. Process with pandas
 df = pd.DataFrame({
@@ -39,11 +74,10 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 
 # 5. Save everything
-if not os.path.exists('data'):
-    os.makedirs('data')
-
-plt.savefig('data/weather_chart.png')
-df.to_csv('data/paris_weather.csv', index=False)
+os.makedirs("output", exist_ok=True)
+plt.savefig("output/dhaka_chart.png")
+df.to_csv("output/dhaka_weather.csv", index=False)
 
 print(f"Average temperature: {df['avg_temp'].mean():.1f}°C")
 print("Files saved in 'data' folder")
+
